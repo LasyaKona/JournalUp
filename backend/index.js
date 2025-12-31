633bc394859d622edd626ddbb93dbc92185b25c9
LasyaKona <konalasya@gmail.com>
2025-12-31 10:49:10 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const  mysql=require('mysql2');
 const connection=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'root@Mysql',
+    password:'root@Mysql123',
     database:'journal'
 });
 connection.connect((err)=>{
